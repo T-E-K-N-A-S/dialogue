7873fdf560d44fc7bb7fd868e8251bc200f3fa8b
teknas <teknas97@gmail.com>
2021-06-30 18:19:20 +0530

    dialogue class and indvidual dialogue  parser

--- parser.py
+++ parser.py
@@ -12,9 +12,28 @@ class SRT():
     def parse(self):
         with open(self.filename,"r", encoding='utf-8-sig') as file:
             data = file.read()
+        data = data.split("\n\n")
+        print("found {} dialoges".format(len(data)))
+        data = [dialogue(d).parse() for d in data if d != ""]
         return data
 
+class dialogue():
+    def __init__(self,d = None) -> None:
+        if d is not None or d != "":
+            self.data = d.split("\n")
+        else:
+            print("please insert a string")
+
+    def parse(self):
+        
+        print("parsing.........",self.data)
+        self.index = int(self.data[0])-1
+        self.time_txt = self.data[1]
+        self.txt = ''.join(s for s in self.data[2:])
+        return self
 
 
 
-
+q = SRT("data/S04E01.srt")
+data = q.parse()
+print("parsed in data")
