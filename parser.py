51769208976c93f75f6395af5f4b5458662fcb43
teknas <teknas97@gmail.com>
2021-06-30 18:46:45 +0530

    parsing timestamps

--- parser.py
+++ parser.py
@@ -2,8 +2,8 @@
 code to parse  srt files
 
 '''
-
-
+from datetime import datetime,timedelta,time
+from dateutil.parser import parse
 
 class SRT():
     def __init__(self,filename) -> None:
@@ -25,11 +25,18 @@ class dialogue():
             print("please insert a string")
 
     def parse(self):
-        
         print("parsing.........",self.data)
         self.index = int(self.data[0])-1
         self.time_txt = self.data[1]
+        time_txt = self.time_txt.split("-->")
+        self.from_time = parse(time_txt[0].strip())
+        self.from_time_txt = self.from_time.strftime('%H:%M:%S::%f')
+        self.to_time = parse(time_txt[1].strip())
+        self.to_time = self.to_time.strftime('%H:%M:%S---%f')
+        # self.display_time = self.to_time - self.from_time
+        # self.display_time_txt = self.display_time.strftime('%M:%S::%f')
         self.txt = ''.join(s for s in self.data[2:])
+        
         return self
 
 
@@ -37,3 +44,5 @@ class dialogue():
 q = SRT("data/S04E01.srt")
 data = q.parse()
 print("parsed in data")
+
+# exec(open("parser.py").read())
