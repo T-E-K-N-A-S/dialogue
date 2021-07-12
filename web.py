68c14fea948dccc04a003c8401b5caacdaeebbc2
teknas <teknas97@gmail.com>
2021-07-12 23:50:32 +0530

    cors in fastapi

--- web.py
+++ web.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from elasticsearch import Elasticsearch
 from dataloader import SRT
 import config
 import json
+
 elastic_client = Elasticsearch(config.ELASTIC_REMOTE_SERVER_URL)
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
@@ -13,6 +27,6 @@ async def root():
 
 @app.get("/dialogue")
 async def root():
-
-    return {"message": "conversation in movies, series & podcasts"}
+    JS = elastic_client.search(index = config.ELASTIC_INDEX)
+    return {"message": "conversation in movies, series & podcasts","JS":JS}
 
